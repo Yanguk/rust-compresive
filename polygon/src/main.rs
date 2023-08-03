d9af2e29cfd1f77fdb223e7254bc3282a42d4d19
Yanguk <yanguk.heo@knowre.com>
2023-08-03 20:49:28 +0900

    Update: polygun 완료

---
+++ polygon/src/main.rs
@@ -0,0 +1,214 @@
+// TODO: remove this when you're done with your implementation.
+#![allow(unused_variables, dead_code)]
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Point {
+    // add fields
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    // add methods
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+
+    fn magnitude(&self) -> f64 {
+        f64::from(self.x.pow(2) + self.y.pow(2)).sqrt()
+    }
+
+    fn dist(&self, point: Point) -> f64 {
+        f64::from((self.x - point.x).pow(2) + (self.y - point.y).pow(2)).sqrt()
+    }
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+pub struct Polygon {
+    // add fields
+    points: Vec<Point>,
+}
+
+impl Polygon {
+    pub fn new() -> Self {
+        Self { points: vec![] }
+    }
+    // add methods
+    pub fn add_point(&mut self, point: Point) {
+        self.points.push(point);
+    }
+
+    pub fn left_most_point(&self) -> Option<Point> {
+        self.points.first().copied()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &Point> {
+        self.points.iter()
+    }
+
+    pub fn length(&self) -> f64 {
+        if self.points.is_empty() {
+            return 0.0;
+        }
+
+        // let mut result = 0.0;
+        //
+        // let mut last_point = self.points[0];
+        //
+        // for point in &self.points[1..] {
+        //     result += last_point.dist(*point);
+        //
+        //     last_point = *point;
+        // }
+        //
+        // result += last_point.dist(self.points[0]);
+
+        let point_pairs = self.points
+            .iter()
+            .zip(self.points.iter().cycle().skip(1));
+
+        // Calculate the total length by summing up the distances between each pair of points.
+        let result: f64 = point_pairs
+            .map(|(p1, p2)| p1.dist(*p2))
+            .sum();
+
+        result
+    }
+}
+
+impl Default for Polygon {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+pub struct Circle {
+    // add fields
+    center: Point,
+    radius: i32,
+}
+
+impl Circle {
+    // add methods
+    pub fn new(center: Point, radius: i32) -> Self {
+        Self { center, radius }
+    }
+
+    pub fn circumference(&self) -> f64 {
+        2.0 * std::f64::consts::PI * f64::from(self.radius)
+    }
+}
+
+pub enum Shape {
+    Polygon(Polygon),
+    Circle(Circle),
+}
+
+impl Shape {
+    pub fn perimeter(&self) -> f64 {
+        match self {
+            Shape::Polygon(poly) => poly.length(),
+            Shape::Circle(circle) => circle.circumference(),
+        }
+    }
+}
+
+impl From<Polygon> for Shape {
+    fn from(value: Polygon) -> Self {
+        Shape::Polygon(value)
+    }
+}
+
+impl From<Circle> for Shape {
+    fn from(value: Circle) -> Self {
+        Shape::Circle(value)
+    }
+}
+
+#[allow(dead_code)]
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn round_two_digits(x: f64) -> f64 {
+        (x * 100.0).round() / 100.0
+    }
+
+    #[test]
+    fn test_point_magnitude() {
+        let p1 = Point::new(12, 13);
+        assert_eq!(round_two_digits(p1.magnitude()), 17.69);
+    }
+
+    #[test]
+    fn test_point_dist() {
+        let p1 = Point::new(10, 10);
+        let p2 = Point::new(14, 13);
+        assert_eq!(round_two_digits(p1.dist(p2)), 5.00);
+    }
+
+    #[test]
+    fn test_point_add() {
+        let p1 = Point::new(16, 16);
+        let p2 = p1 + Point::new(-4, 3);
+        assert_eq!(p2, Point::new(12, 19));
+    }
+
+    #[test]
+    fn test_polygon_left_most_point() {
+        let p1 = Point::new(12, 13);
+        let p2 = Point::new(16, 16);
+
+        let mut poly = Polygon::new();
+        poly.add_point(p1);
+        poly.add_point(p2);
+        assert_eq!(poly.left_most_point(), Some(p1));
+    }
+
+    #[test]
+    fn test_polygon_iter() {
+        let p1 = Point::new(12, 13);
+        let p2 = Point::new(16, 16);
+
+        let mut poly = Polygon::new();
+        poly.add_point(p1);
+        poly.add_point(p2);
+
+        let points = poly.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(points, vec![Point::new(12, 13), Point::new(16, 16)]);
+    }
+
+    #[test]
+    fn test_shape_perimeters() {
+        let mut poly = Polygon::new();
+        poly.add_point(Point::new(12, 13));
+        poly.add_point(Point::new(17, 11));
+        poly.add_point(Point::new(16, 16));
+
+        let shapes = vec![
+            Shape::from(poly),
+            Shape::from(Circle::new(Point::new(10, 20), 5)),
+        ];
+
+        let perimeters = shapes
+            .iter()
+            .map(Shape::perimeter)
+            .map(round_two_digits)
+            .collect::<Vec<_>>();
+
+        assert_eq!(perimeters, vec![15.48, 31.42]);
+    }
+}
